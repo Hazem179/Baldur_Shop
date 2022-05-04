0bb0f282b65388dad47f32bfe370fa148cadf169
Hazem <hazemm913@gmail.com>
2022-05-04 06:28:06 +0200

    payment

--- Baldur_shop/urls.py
+++ Baldur_shop/urls.py
@@ -19,10 +19,12 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('payment/', include('payment.urls', namespace='payment')),
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
     path('', include('shop.urls', namespace='shop')),
     path('orders/', include('orders.urls', namespace='orders')),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
