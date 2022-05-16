251c51b48ad66d7ad853023119b658c8da1fa807
Hazem <hazemm913@gmail.com>
2022-05-16 13:45:45 +0200

    coupons system

--- orders/admin.py
+++ orders/admin.py
@@ -2,6 +2,8 @@ import csv
 import datetime
 from django.http import HttpResponse
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 from .models import Order, OrderItem
 
 
@@ -10,6 +12,24 @@ from .models import Order, OrderItem
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
+
+
+# def order_pdf(obj):
+#     url = reverse('orders:admin_order_pdf', args=[obj.id])
+#     return mark_safe(f'<a href="{url}">PDF</a>')
+
+
+# order_pdf.short_description = 'Invoice PDF'
+
+'''
+This is a function that takes an Order object as an argument and returns an HTML 
+link for the admin_order_detail URL
+'''
+
+
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
 
 
 def export_to_csv(modeladmin, request, queryset):
@@ -32,7 +52,7 @@ def export_to_csv(modeladmin, request, queryset):
 
     '''
     fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
-    writer.writerow([field.verbose_name for field in fields]) # write header row including field names
+    writer.writerow([field.verbose_name for field in fields])  # write header row including field names
     '''
     You iterate over the given QuerySet and write a row for each object returned
     by the QuerySet. You take care of formatting datetime objects because the
@@ -48,37 +68,14 @@ def export_to_csv(modeladmin, request, queryset):
         writer.writerow(data_row)
     return response
 
-export_to_csv.short_description = 'Export to CSV' # customize display name
 
-
-# def export_to_csv(modeladmin, request, queryset):
-#     opts = modeladmin.model._meta
-#     content_disposition = 'attachment; filename={opts.verbose_name}.csv'
-#     response = HttpResponse(content_type='text/csv')
-#     response['Content-Disposition'] = content_disposition
-#     writer = csv.writer(response)
-#     fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
-#     # Write a first row with header information
-#     writer.writerow([field.verbose_name for field in fields])
-#     # Write data rows
-#     for obj in queryset:
-#         data_row = []
-#         for field in fields:
-#             value = getattr(obj, field.name)
-#             if isinstance(value, datetime.datetime):
-#                 value = value.strftime('%d/%m/%Y')
-#             data_row.append(value)
-#         writer.writerow(data_row)
-#     return response
-
-
-export_to_csv.short_description = 'Export to CSV'
+export_to_csv.short_description = 'Export to CSV'  # customize display name
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
-                    'address', 'postal_code', 'city', 'paid', 'created', 'updated']
+                    'address', 'postal_code', 'city', 'paid', 'created', 'updated', order_detail]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
