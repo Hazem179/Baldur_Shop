a7ea7786d812748d65e1056b23ef9b82a2ef1117
Hazem <hazemm913@gmail.com>
2022-05-16 15:12:24 +0200

    coupons system mod

--- orders/views.py
+++ orders/views.py
@@ -17,7 +17,11 @@ def order_create(request):
     if request.method == 'POST':
         form = OrderCreateForm(request.POST)
         if form.is_valid():
-            order = form.save()
+            order = form.save(commit=False)
+            if cart.coupon:
+                order.coupon = cart.coupon
+                order.discount = cart.coupon.discount
+            order.save()
             for item in cart:
                 OrderItem.objects.create(
                     order=order,
@@ -38,7 +42,6 @@ def order_create(request):
 def admin_order_detail(request, order_id):
     order = get_object_or_404(Order, id=order_id)
     return render(request, 'admin/orders/order/detail.html', {'order': order})
-
 
 # @staff_member_required
 # def admin_order_pdf(request, order_id):
